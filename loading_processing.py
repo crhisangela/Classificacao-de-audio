f58d52fb1d0358d792753a500ed77b844e749240
Crhis Ferreira <ferreiracrhis@gmail.com>
2023-02-06 20:45:02 -0300

    mono and stereo
    carregamento e processamento de audios mono, stereos e criacao de clicks nos picos

---
+++ loading_processing.py
@@ -0,0 +1,11 @@
+import numpy as np
+import time
+from datetime import datetime
+import math
+import matplotlib.pyplot as plt
+plt.rcParams['figure.fgsize'] = (14,6)  #definfindo tamanho da imagem padrao
+from IPython.display import HTML, Audio
+import librosa
+import librosa.display as ld
+import seaborn as sn
+sn.set()
